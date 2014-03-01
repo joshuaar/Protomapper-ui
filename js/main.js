f6d01d47f7dd8d812bb42f791a76d7011641a461
josh <joshamosricher@gmail.com>
2014-03-01 14:56:17 -0700

    autopush

--- js/main.js
+++ js/main.js
@@ -198,6 +198,11 @@ var paginationCtrl = function ($scope, results) {
 
 //Grid with pagination for results
 app.controller('GridCtrl', function($scope, $http, results) {
+    $scope.myData = [{name: "Moroni", age: 50},
+        {name: "Tiancum", age: 43},
+        {name: "Jacob", age: 27},
+        {name: "Nephi", age: 29},
+        {name: "Enos", age: 34}]
     $scope.filterOptions = {
         filterText: "",
         useExternalFilter: true
