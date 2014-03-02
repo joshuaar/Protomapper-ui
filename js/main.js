8f5240b1ecef1d71ad3542d79108476dd1153555
josh <joshamosricher@gmail.com>
2014-03-01 17:29:14 -0700

    autopush

--- js/main.js
+++ js/main.js
@@ -218,6 +218,7 @@ app.controller('GridCtrl', function($scope,$http,ngTableParams,results) {
         {name: "Enos", age: 34}];
     $http.get('/query?q=AVHAD&r=0%%50').success(function (largeLoad) {
          data = largeLoad["res"]
+         $scope.tableParams.total(data.length)
          $scope.tableParams.reload()
     });
     $scope.tableParams = new ngTableParams({
