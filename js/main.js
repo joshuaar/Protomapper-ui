6bf3bb9cc1d31865aff6e98d2ed314c8f195d68a
josh <joshamosricher@gmail.com>
2014-03-01 17:23:09 -0700

    autopush

--- js/main.js
+++ js/main.js
@@ -224,7 +224,7 @@ app.controller('GridCtrl', function($scope,$http,ngTableParams,results) {
         page: 1,            // show first page
         count: 10           // count per page
     }, {
-        total: data.length, // length of data
+        total: function () { return data.length; }, // length of data
         getData: function($defer, params) {
             $defer.resolve(data.slice((params.page() - 1) * params.count(), params.page() * params.count()));
         }
